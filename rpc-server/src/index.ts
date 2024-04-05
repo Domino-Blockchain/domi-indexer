2506b7dc39afe44387c8e7290f09104a866aa741
Artyom Titov <at@redsteep.com>
2024-04-05 22:48:49 +0300

    fix: fix CORS

--- rpc-server/src/index.ts
+++ rpc-server/src/index.ts
@@ -1,6 +1,10 @@
+import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
 import { inscriptionsRouter } from "~/resources/inscriptions";
 
-const app = new Elysia().use(inscriptionsRouter).listen(process.env.PORT ?? 4000);
+const app = new Elysia()
+  .use(cors())
+  .use(inscriptionsRouter)
+  .listen(process.env.PORT ?? 4000);
 
 console.log(`🦊 Indexer RPC is running at ${app.server?.hostname}:${app.server?.port}`);
